c61b99df803eeafac8e1ce7b96decd2778648713
Bogdan Lozev <lozev.bogdan@abv.bg>
2024-03-29 16:35:14 +0200

    Initial commit

---
+++ src/main/java/com/cashdesh/cashdesh/module/CashDeshModuleApplication.java
@@ -0,0 +1,13 @@
+package com.cashdesh.cashdesh.module;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashDeshModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashDeshModuleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cashdesh/cashdesh/module/CashDeshModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cashdesh.cashdesh.module;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashDeshModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
